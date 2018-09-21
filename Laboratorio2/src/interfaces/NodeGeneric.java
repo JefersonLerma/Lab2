8e123a68535a0d5ca834e2109d8a2bf68826e6ca
jeferson <jeferson.lerma@correo.icesi.edu.co>
2018-09-20 21:59:30 -0500

    I did the generic node interface methods

--- Laboratorio2/src/interfaces/NodeGeneric.java
+++ Laboratorio2/src/interfaces/NodeGeneric.java
@@ -1,5 +1,10 @@
 package interfaces;
 
-public interface NodeGeneric {
-
+public interface NodeGeneric<T> {
+	
+	
+	public NodeGeneric<T> giveNext();
+	public NodeGeneric<T> givePrevious();
+    public void changeNext(NodeGeneric<T> node);
+    public void changePreviuos(NodeGeneric<T> node);
 }
