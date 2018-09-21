d3e8354b9d68e722b57d6745fd89a3f3939463f4
jeferson <jeferson.lerma@correo.icesi.edu.co>
2018-09-20 23:02:22 -0500

    Add the dashboard data and the stack methods.

--- Laboratorio2/src/interfaces/Stack.java
+++ Laboratorio2/src/interfaces/Stack.java
@@ -1,7 +1,16 @@
 package interfaces;
+import model.*;
 
-public interface Stack {
+public interface Stack<T> {
 	
+	//Apilar
+	public void stackUp(T t);
 	
+	public boolean noWeapons();
+	
+	//desapilar
+	public T unStack();
+	
+	public Node<T> getTop();
 
 }
--- Laboratorio2/src/model/StackWeapon.java
+++ Laboratorio2/src/model/StackWeapon.java
@@ -1,6 +1,51 @@
 package model;
 
-public class StackWeapon {
+import interfaces.Stack;
+
+public class StackWeapon<T> implements Stack<T> {
+
+	private Node<T> firstStack;
+	
+	public StackWeapon() {
+		// TODO Auto-generated constructor stub
+		firstStack = null;
+	}
+	@Override
+	public void stackUp(T t) {
+		// TODO Auto-generated method stub
+		Node<T> node = new Node<T>(t); 
+		if(noWeapons()) {
+			firstStack = node;
+		}else {
+			Node<T> current = firstStack;
+			
+			while(current.giveNext()!= null) {
+			current = (Node<T>) current.giveNext();	
+			}
+				current.changeNext(node);
+				node.changePreviuos(current);
+				node.changeNext(current);
+				
+		}
+	}
+
+	@Override
+	public boolean noWeapons() {
+		// TODO Auto-generated method stub
+		return firstStack == null;
+	}
+
+	@Override
+	public T unStack() {
+	return null;
+	
+	}
+
+	@Override
+	public Node<T> getTop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 
 }
