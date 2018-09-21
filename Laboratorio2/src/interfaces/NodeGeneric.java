47d6fb3f9e9059ac4b06b3ab4138ed7be32fa6e9
jeferson <jeferson.lerma@correo.icesi.edu.co>
2018-09-20 22:17:17 -0500

    I implemented the methods for the generic node

--- Laboratorio2/src/interfaces/NodeGeneric.java
+++ Laboratorio2/src/interfaces/NodeGeneric.java
@@ -1,10 +1,11 @@
 package interfaces;
+import model.*;
 
 public interface NodeGeneric<T> {
 	
 	
 	public NodeGeneric<T> giveNext();
 	public NodeGeneric<T> givePrevious();
-    public void changeNext(NodeGeneric<T> node);
-    public void changePreviuos(NodeGeneric<T> node);
+    public void changeNext(Node<T> node);
+    public void changePreviuos(Node<T> node);
 }
--- Laboratorio2/src/model/Node.java
+++ Laboratorio2/src/model/Node.java
@@ -1,5 +1,44 @@
 package model;
 
-public class Node {
+import interfaces.NodeGeneric;
+// last to enter, first out
+public class Node<T> implements NodeGeneric<T> {
+
+	private Node<T> objectNext;
+	private Node<T> objectPrevious;
+	private T object;
+	
+	public Node(T pObjet) {
+		// TODO Auto-generated constructor stub
+		object = pObjet;
+		objectNext = null;
+		objectPrevious = null;
+	}
+	
+	
+	@Override
+	public NodeGeneric<T> giveNext() {
+		// TODO Auto-generated method stub
+		return objectNext;
+	}
+
+	@Override
+	public NodeGeneric<T> givePrevious() {
+		// TODO Auto-generated method stub
+		return objectPrevious;
+	}
+
+	@Override
+	public void changeNext(Node<T> node) {
+		// TODO Auto-generated method stub
+		objectNext = node;
+		
+	}
+
+	@Override
+	public void changePreviuos(Node<T> node) {
+		// TODO Auto-generated method stub
+		objectPrevious = node;
+	}
 
 }
